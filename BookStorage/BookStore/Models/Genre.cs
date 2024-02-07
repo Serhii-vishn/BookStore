2d6c240f36ce16f2db55784b7df840a56013340f
Serhii <66913762+Serhii-vishn@users.noreply.github.com>
2024-02-07 23:46:40 +0200

    Add models

---
+++ BookStorage/BookStore/Models/Genre.cs
@@ -0,0 +1,10 @@
+﻿namespace Books.Models
+{
+    public class Genre
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = null!;
+
+        public List<Book> Books { get; set; }
+    }
+}
