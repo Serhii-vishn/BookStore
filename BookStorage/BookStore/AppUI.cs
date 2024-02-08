c367ffc0d33ffd5f0433489cd88650bf70faa79c
Serhii <66913762+Serhii-vishn@users.noreply.github.com>
2024-02-08 22:38:50 +0200

    Add class AppUI

---
+++ BookStorage/BookStore/AppUI.cs
@@ -0,0 +1,39 @@
+﻿namespace BookStore
+{
+    public static class AppUI
+    {
+        public static char GetMenuOption()
+        {
+            char menuOption;
+            do
+            {
+                Console.WriteLine("\nAdd Books from File - 1");
+                Console.WriteLine("Search Books - 2");
+
+                Console.Write("Enter: ");
+                menuOption = Console.ReadKey().KeyChar;
+            } while (!menuOption.Equals('1') && !menuOption.Equals('2'));
+
+            return menuOption;
+        }
+
+        public static string GetFilePathFromUser()
+        {
+            string filePath;
+            do
+            {
+                Console.Write("\nEnter file path: ");
+                filePath = Console.ReadLine();
+
+                if (!File.Exists(filePath))
+                {
+                    Console.WriteLine("Error: The entered file path is invalid or doesn't exist.");
+                }
+            } while (!File.Exists(filePath));
+
+            return filePath;
+        }
+
+
+    }
+}
