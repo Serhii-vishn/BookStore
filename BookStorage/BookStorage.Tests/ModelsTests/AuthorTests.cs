0684ff75fcaa003c1f8ae3619e467f4480c4d3a8
Serhii <66913762+Serhii-vishn@users.noreply.github.com>
2024-02-13 22:17:57 +0200

    Added ModelTests

---
+++ BookStorage/BookStorage.Tests/ModelsTests/AuthorTests.cs
@@ -0,0 +1,24 @@
+﻿using Books.Models;
+namespace Models.Tests
+{
+    [TestFixture]
+    public class AuthorTests
+    {
+        [Test]
+        public void AuthorProperties_CanBeSetAndGet()
+        {
+            var author = new Author
+            {
+                Id = Guid.NewGuid(),
+                Name = "Test Author",
+            };
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(author.Name, Is.EqualTo("Test Author"));
+                Assert.That(author.Id, Is.Not.EqualTo(null));
+                Assert.That(author.Books, Is.EqualTo(null));
+            });
+        }
+    }
+}
---
+++ BookStorage/BookStorage.Tests/ModelsTests/GenreTests.cs
@@ -0,0 +1,25 @@
+﻿using Books.Models;
+
+namespace Models.Tests
+{
+    [TestFixture]
+    public class GenreTests
+    {
+        [Test]
+        public void GenreProperties_CanBeSetAndGet()
+        {
+            var genre = new Genre
+            {
+                Id = Guid.NewGuid(),
+                Name = "Test Genre",
+            };
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(genre.Name, Is.EqualTo("Test Genre"));
+                Assert.That(genre.Id, Is.Not.EqualTo(null));
+                Assert.That(genre.Books, Is.EqualTo(null));
+            });
+        }
+    }
+}
