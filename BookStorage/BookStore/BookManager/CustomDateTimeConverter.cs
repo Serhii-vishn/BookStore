da4522e3cec4bc1fd73db889511d7bfaa7f68ac0
Serhii <66913762+Serhii-vishn@users.noreply.github.com>
2024-02-13 22:02:04 +0200

    add BookModel from csv and maping

---
+++ BookStorage/BookStore/BookManager/CustomDateTimeConverter.cs
@@ -0,0 +1,21 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using CsvHelper.TypeConversion;
+using System.Globalization;
+
+namespace BookStore.BookManager
+{
+    public class CustomDateTimeConverter : DateTimeConverter
+    {
+        public override object ConvertFromString(string text, IReaderRow row, MemberMapData memberMapData)
+        {
+            if (text.ToLower().Contains("century bc"))
+            {
+                int century = int.Parse(text.ToLower().Replace("th century bc", "").Replace("st century bc", "").Replace("nd century bc", "").Replace("rd century bc", ""));
+                int year = (century - 1) * 100;
+                return new DateTime(year, 1, 1, new GregorianCalendar());
+            }
+            return base.ConvertFromString(text, row, memberMapData);
+        }
+    }
+}
