5f60a36746268a8f37333d3e723354f12cca1daf
Serhii <66913762+Serhii-vishn@users.noreply.github.com>
2024-02-13 22:12:26 +0200

    added services

---
+++ BookStorage/BookStore/Services/AuthorService.cs
@@ -0,0 +1,26 @@
+﻿using Books.Data;
+using Books.Models;
+
+namespace BookStore.Services
+{
+    public class AuthorService
+    {
+        public Author FindOrCreateAuthor(string name, BookContext db)
+        {
+            var existingAuthor = db.Authors.FirstOrDefault(a => a.Name == name);
+
+            if (existingAuthor != null)
+            {
+                return existingAuthor;
+            }
+            else
+            {
+                var newAuthor = new Author { Name = name };
+                db.Authors.Add(newAuthor);
+                db.SaveChanges();
+
+                return newAuthor;
+            }
+        }
+    }
+}
